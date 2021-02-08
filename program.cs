bd6651349b528bdfd52c638578104649e6126370
LAPTOP-Q44406F1\Diana <diana.dsantos2@gmail.com>
2021-02-08 15:36:18 -0500

    Update

---
+++ program.cs
@@ -0,0 +1,71 @@
+// ------------------- Class Battery ---------------------------
+class Battery {
+    public int ID;
+    public int amountOfColumns;
+    public int status;
+    public int amountOfFloors;
+    public int amountOfBasements;
+    public callButtonsList;//(List of CallButton objects) //Moderne seulement 
+    public List<Column> listColumnInBattery;//List of Column objects
+    public Battery(int id, int nbOfColumnInBattery, int nbOfBasement, List<Column> listColumnInBattery)
+        {
+            this.id = id;
+            this.nbOfColumnInBattery = nbOfColumnInBattery;
+            this.nbOfBasement = nbOfBasement;
+            this.listColumnInBattery = listColumnInBattery;
+        } 
+}
+
+// mandatory functions - Moderne seulement : assignElevator(requestedFloor, direction) and findBestColumn(requestedFloor). Devrait retourner un objet Column
+
+
+// ------------------- Class Column ---------------------------
+class Column(id, status, amountOfFloors, amountOfElevators, servedFloors, isBasement) {
+
+    public int ID;
+    public int status; 
+    public servedFloors; //(List of floors)
+    public int amountOfElevators;
+    public elevatorsList; //(Liste d’objets Elevator)
+    public callButtonsList; //(Liste d’objets CallButton) Classique seulement
+    public floorRequestButtonsList; //(Liste d’objets FloorRequestButton) Moderne seulement
+
+    }
+
+   // mandatory functions: requestElevator (requestedFloor, direction).
+
+// ------------------- Class Elevators  ---------------------------
+class Elevator(id, status, amountOfFloors, currentFloor) {
+    public int ID;
+    public int status;
+    public int currentFloor;
+    public int direction;
+    public int door; //Door object
+    public floorRequestButtonsList; //Liste d’objets FloorRequestButton  -  Classique seulement
+    public floorRequestList:
+
+    //mandatory functions: requestFloor(requestedFloor).-- Classique seulement
+
+
+
+}
+
+// ------------------- Class CallButton ---------------------------
+class CallButton(id, status, floor, direction){
+    public int ID;
+    public int status;
+    public int floor;
+    public int direction
+}
+
+// ------------------- Class CallButton ---------------------------
+class FloorRequestButton(id, status, floor){
+    public int ID;
+    public int status;
+    public int floor
+}
+// ------------------- Class CallButton ---------------------------
+class Door(id, status){
+    public int ID;
+    public int status;
+}
